b9d80093c868f61e29f102422cb3f02ca8ca8eb5
vadim <lociX55Lim@gmail.com>
2024-06-30 13:08:23 +0300

    feat: add change hostname func

--- client/cmd/changehostfunc.go
+++ client/cmd/changehostfunc.go
@@ -1,6 +1,20 @@
 package cmd
 
-func changeHostname(hostname string) error {
-  //TODO after service
-  return nil
+import (
+	"context"
+	"github.com/charmbracelet/log"
+	"github.com/lilpipidron/Host-DNS-Updater/proto/service/hostname"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func changeHostname(newHostname string) error {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Error("did not connect: %v", err)
+	}
+	defer conn.Close()
+	hostnameClient := hostname.NewHostnameServiceClient(conn)
+	_, err = hostnameClient.SetHostName(context.Background(), &hostname.SetHostnameRequest{Hostname: newHostname})
+	return err
 }
