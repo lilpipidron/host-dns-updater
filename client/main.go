629ce178e68d1f3219560847f015bf8e66a6fe4a
vadim <lociX55Lim@gmail.com>
2024-06-30 13:24:30 +0300

    refactor: change root cmd

--- client/main.go
+++ client/main.go
@@ -1,25 +1,9 @@
 package main
 
 import (
-	"github.com/charmbracelet/log"
 	"github.com/lilpipidron/Host-DNS-Updater/client/cmd"
-	"github.com/lilpipidron/Host-DNS-Updater/proto/service/dns"
-	"github.com/lilpipidron/Host-DNS-Updater/proto/service/hostname"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
-var dnsClient *dns.DNSServiceClient
-var hostnameClient *hostname.HostnameServiceClient
-
 func main() {
-	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Error("did not connect: %v", err)
-	}
-	defer conn.Close()
-	dnsClient := dns.NewDNSServiceClient(conn)
-	hostnameClient := hostname.NewHostnameServiceClient(conn)
-
 	cmd.Execute()
 }
