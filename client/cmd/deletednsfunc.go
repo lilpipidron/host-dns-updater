41f679d42763565238918bd51f5b1fe3d8575312
vadim <lociX55Lim@gmail.com>
2024-06-30 13:06:07 +0300

    feat: complete delete dns func

--- client/cmd/deletednsfunc.go
+++ client/cmd/deletednsfunc.go
@@ -1,6 +1,20 @@
 package cmd
 
-func deleteDNS(dns string) error {
-	// TODO after service
-	return nil
+import (
+	"context"
+	"github.com/charmbracelet/log"
+	"github.com/lilpipidron/Host-DNS-Updater/proto/service/dns"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func deleteDNS(dnsForDelete string) error {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Error("did not connect: %v", err)
+	}
+	defer conn.Close()
+	dnsClient := dns.NewDNSServiceClient(conn)
+	_, err = dnsClient.DeleteDNS(context.Background(), &dns.DeleteDNSRequest{Dns: dnsForDelete})
+	return err
 }
